27ca2c1e69e1ab640c367120a8ef1f1cddea8dd0
SB44444 <mega.s.style@gmail.com>
2023-04-06 04:56:18 +0300

    Seminar 5 OOP Java had been done

---
+++ gb-notebook_Seminar5/gb-notebook_S5/src/notebook/view/mapper/Mapper.java
@@ -0,0 +1,11 @@
+package notebook.view.mapper;
+
+/**
+ * ��������� ������� �� ������ ���� � ������.
+ * @param <E> ���, �������� � ��
+ * @param <T> ���, ���������� ��� ����������� E
+ */
+public interface Mapper<E, T> {
+    T toInput(E e);
+    E toOutput(T t);
+}
